785a7f6c1233acd7cab1ea0649e99f5c0ae5f8fa
Andrew Tamez <147640989+ATamez92@users.noreply.github.com>
2026-02-27 16:42:33 -0600

    Implement application shell layout and primary navigation structure

--- Omnium.UI/obj/Debug/net8.0-windows/Omnium.UI.AssemblyInfo.cs
+++ Omnium.UI/obj/Debug/net8.0-windows/Omnium.UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Omnium.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f31e68eba4d8d6891fa2a830c2f706b90de8dd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc0185600ecda60de3aa59ed5038e61e5e0cb505")]
 [assembly: System.Reflection.AssemblyProductAttribute("Omnium.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Omnium.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
